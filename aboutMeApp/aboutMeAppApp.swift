59ad18463491570fd5882836bfbcea35fbe489b5
Tess <tessaghuang@gmail.com>
2023-06-23 14:59:22 -0400

    Initial Commit

---
+++ aboutMeApp/aboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeAppApp.swift
+//  aboutMeApp
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
